59af6a5d63df6156614927cabe1ccd4d92abb112
Alex P <yinjiat.phoon@gmail.com>
2018-10-16 20:00:40 +0800

    Allow admins to upload agent cert

--- functions/index.js
+++ functions/index.js
@@ -275,35 +275,17 @@ exports.processPostageReload = functions.database.ref('/reloadPostageRequests/{r
   return retVal;
 });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-// exports.addMessage = functions.https.onRequest((req, res) => {
-//   // Grab the text parameter.
-//   const original = req.query.text;
-//   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-//   return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-//     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-//     return res.redirect(303, snapshot.ref.toString());
-//   });
-// });
-
-// // Listens for new messages added to /messages/:pushId/original and creates an
-// // uppercase version of the message to /messages/:pushId/uppercase
-// exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-//     .onCreate((snapshot, context) => {
-//       // Grab the current value of what was written to the Realtime Database.
-//       const original = snapshot.val();
-//       console.log('Uppercasing', context.params.pushId, original);
-//       const uppercase = original.toUpperCase();
-//       // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//       // writing to the Firebase Realtime Database.
-//       // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-//       return snapshot.ref.parent.child('uppercase').set(uppercase);
-//     });
+// Remove old cert image if it's updated
+exports.onCertWrite = functions.database.ref('/users/{userId}/cert').onWrite((change, context) => {
+  var retVal = null;
+  if (change.before.exists()) {
+    // Delete attached image
+    const before = change.before.val();
+    if (before.cert) {
+      // Delete the photo
+      var imagePath = before.cert.path;
+      retVal = admin.storage().bucket().file(imagePath).delete();
+    }
+  }
+  return retVal;
+});
